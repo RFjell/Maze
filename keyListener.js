e6d8ee61919c672d812d6279f05c5ce66c63746a
Robin <robban5000@gmail.com>
2017-02-03 22:32:49 +0100

    Added cheat to move through walls when holding shift and moving

--- keyListener.js
+++ keyListener.js
@@ -4,28 +4,28 @@ function doKeyDown(e){
 	}
 	switch (e.keyCode) {
 		case 38:  /* Up arrow */
-			if( playerPosition[1] > 0 && !grid[playerPosition[0]][playerPosition[1]-1].south ) {
+			if( playerPosition[1] > 0 && (!grid[playerPosition[0]][playerPosition[1]-1].south || e.shiftKey) ) {
 				clearPlayer();
 				playerPosition[1] -= 1;
 				drawPlayer();
 			}
 			break;
 		case 40:  /* Down arrow */
-			if( playerPosition[1] < height - 1 && !grid[playerPosition[0]][playerPosition[1]].south ) {
+			if( playerPosition[1] < height - 1 && (!grid[playerPosition[0]][playerPosition[1]].south || e.shiftKey)  ) {
 				clearPlayer();
 				playerPosition[1] += 1;
 				drawPlayer();
 			}
 			break;
 		case 37:  /* Left arrow */
-			if( playerPosition[0] > 0 && !grid[playerPosition[0]][playerPosition[1]].west ) {
+			if( playerPosition[0] > 0 && (!grid[playerPosition[0]][playerPosition[1]].west || e.shiftKey)  ) {
 				clearPlayer();
 				playerPosition[0] -= 1;
 				drawPlayer();
 			}
 			break;
 		case 39:  /* Right arrow */
-			if( playerPosition[0] < width - 1 && !grid[playerPosition[0]+1][playerPosition[1]].west ) {
+			if( playerPosition[0] < width - 1 && (!grid[playerPosition[0]+1][playerPosition[1]].west || e.shiftKey) ) {
 				clearPlayer();
 				playerPosition[0] += 1;
 				drawPlayer();
